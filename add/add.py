08252cd2f57b963be432b667e49bb222fc96eaeb
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2021-09-09 11:24:04 +0200

    refactor pythonic

--- add/add.py
+++ add/add.py
@@ -1,8 +1,6 @@
 def add(m1: list[list], m2: list[list]) -> list[list]:
     result = []
     for idx1, val1 in enumerate(m1):
-        sub_val = []
-        for idx2, val2 in enumerate(val1):
-            sub_val.append(val2 + m2[idx1][idx2])
+        sub_val = [val2 + m2[idx1][idx2] for idx2, val2 in enumerate(val1)]
         result.append(sub_val)
     return result
