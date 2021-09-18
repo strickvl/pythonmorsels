8260ff35245e0db99fa807798524372bcfbc8c64
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2021-09-18 11:44:37 +0200

    solve all parts of the problem

--- circle/circle.py
+++ circle/circle.py
@@ -3,15 +3,34 @@ from math import pi
 
 class Circle:
     def __init__(self, radius=1):
-        self.radius = radius
+        if radius < 0:
+            raise ValueError('Radius cannot be negative')
+        self.__radius = radius
 
     @property
     def diameter(self):
-        return self.radius * 2
+        return self.__radius * 2
+
+    @property
+    def radius(self):
+        return self.__radius
+
+    @radius.setter
+    def radius(self, value):
+        if value < 0:
+            raise ValueError('Radius cannot be negative')
+        else:
+            self.__radius = value
+
+    @diameter.setter
+    def diameter(self, diameter):
+        if diameter < 0:
+            raise ValueError('Diameter cannot be negative')
+        self.__radius = diameter / 2
 
     @property
     def area(self):
-        return pi * (self.radius * self.radius)
+        return pi * (self.__radius * self.__radius)
 
     def __repr__(self):
-        return f'Circle({self.radius})'
+        return f'Circle({self.__radius})'
