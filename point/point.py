f98baa4ec21ca619225c29db7ed842df2613c3cc
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2021-09-18 12:34:21 +0200

    solve point problem and all bonus exercises

--- point/point.py
+++ point/point.py
@@ -4,8 +4,25 @@ class Point:
         self.y = y
         self.z = z
 
+    def __iter__(self):
+        yield self.x
+        yield self.y
+        yield self.z
+
     def __repr__(self):
         return f"Point(x={self.x}, y={self.y}, z={self.z})"
 
     def __eq__(self, other):
-        return (self.x == other.x and self.y == other.y and self.z == other.z)
+        return (self.x, self.y, self.z) == (other.x, other.y, other.z)
+
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y, self.z + other.z)
+
+    def __sub__(self, other):
+        return Point(self.x - other.x, self.y - other.y, self.z - other.z)
+
+    def __mul__(self, val: int):
+        return Point(self.x * val, self.y * val, self.z * val)
+
+    def __rmul__(self, val: int):
+        return Point(self.x * val, self.y * val, self.z * val)
