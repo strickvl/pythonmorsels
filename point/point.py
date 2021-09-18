9a9a790c0a886795cb74a6b384ab76a5322d4d7f
Alex Strick van Linschoten <strickvl@users.noreply.github.com>
2021-09-18 12:48:30 +0200

    refactor based on new iterator

--- point/point.py
+++ point/point.py
@@ -13,7 +13,7 @@ class Point:
         return f"Point(x={self.x}, y={self.y}, z={self.z})"
 
     def __eq__(self, other):
-        return (self.x, self.y, self.z) == (other.x, other.y, other.z)
+        return tuple(self) == tuple(other)
 
     def __add__(self, other):
         return Point(self.x + other.x, self.y + other.y, self.z + other.z)
